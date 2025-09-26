79255e4280764f7a6b2cb43b87cc1763ecad47aa
Kent <kentmanayon@gmail.com>
2025-09-26 20:13:13 +0800

    changes midact1 code

--- Apps.js
+++ Apps.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ChatScreen from "./ChatScreen";
+import ChatBubble from "./ChatBubble";
 import CommentSection from "./CommentSection";
 
 export default function App() {
   return (
     <>
       <ChatScreen />
+      <ChatBubble />
      <CommentSection />
     </>
   );
